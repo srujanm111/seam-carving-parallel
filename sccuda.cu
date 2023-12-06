ceaee2dddb17fcb12a03eca03dea62bdf28d18c5
Kiran Chandrasekhar <61947185+kiranchandrasekhar@users.noreply.github.com>
2023-12-06 15:11:01 -0600

    Update sccuda.cu

--- sccuda.cu
+++ sccuda.cu
@@ -3,6 +3,7 @@
 #include "matrix.hpp"
 
 #include <iostream>
+#include <assert.h>
 
 #include <cuda_runtime.h>
 #include <cuda_profiler_api.h>
@@ -10,6 +11,16 @@
 #define BLOCK_DIM 16
 #define THREADS_PER_BLOCK 256
 #define NUM_COLORS 3
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
 
 int find_cuda_device() {
     int numDevices = 0;
@@ -36,11 +47,11 @@ __device__ float* get_pixel(float* image, int width, int x, int y) {
 }
 
 __device__ float x_gradient(float* image, int height, int width, int x, int y) {
-  int x_left = x != 0 ? x - 1 : width - 1;
-  int x_right = x != width - 1 ? x + 1 : 0;
+  int x_up = x == 0 ? height - 1 : x - 1;
+  int x_down = x == height - 1 ? 0 : x + 1;
 
-  float* left = get_pixel(image, width, x_left, y);
-  float* right = get_pixel(image, width, x_right, y);
+  float* left = get_pixel(image, width, x_up, y);
+  float* right = get_pixel(image, width, x_down, y);
 
   float r = powf(left[0] - right[0], 2);
   float g = powf(left[1] - right[1], 2);
@@ -50,11 +61,11 @@ __device__ float x_gradient(float* image, int height, int width, int x, int y) {
 }
 
 __device__ float y_gradient(float* image, int height, int width, int x, int y) {
-  int y_above = y != 0 ? y - 1 : height - 1;
-  int y_below = y != height - 1 ? y + 1 : 0;
+  int y_left = y == 0 ? width - 1 : y - 1;
+  int y_right = y == width - 1 ? 0 : y + 1;
 
-  float* above = get_pixel(image, width, y_above, y);
-  float* below = get_pixel(image, width, y_below, y);
+  float* above = get_pixel(image, width, x, y_left);
+  float* below = get_pixel(image, width, x, y_right);
 
   float r = powf(above[0] - below[0], 2);
   float g = powf(above[1] - below[1], 2);
@@ -72,7 +83,7 @@ __global__ void dual_gradient(float* image, int height, int width, float* energi
     float y_grad = y_gradient(image, height, width, x, y);
 
     int energy_idx = y * width + x;
-    energies[energy_idx]  = x_grad + y_grad + 1.0;
+    energies[energy_idx] = x_grad + y_grad + 1.0;
   }
 }
 
@@ -85,19 +96,16 @@ __global__ void min_cost(float* energies, int width, int row, float* min_energie
   int y = row;
   int x = blockIdx.x * blockDim.x + threadIdx.x;
 
-  printf("%d\n", x);
-
   if(x < width) {
     float min_energy_above = 0;
     if(y > 0) {
-      min_energy_above = get_energy(energies, width, x, y - 1);
-      if(x > 0) {
-        float left = get_energy(energies, width, x - 1, y - 1);
-        min_energy_above = min(min_energy_above, left);
-      }
-      if(x < width - 1) {
-        float right = get_energy(energies, width, x + 1, y - 1);
-        min_energy_above = min(min_energy_above, right);
+      for(int k = x - 1; k <= x + 1; k++) {
+        if (k >= 0 && k < width) {
+          float energy = get_energy(min_energies, width, k, y - 1);
+          if (min_energy_above == 0 || energy < min_energy_above) {
+            min_energy_above = energy;
+          }
+        }
       }
     }
 
@@ -111,69 +119,83 @@ __global__ void min_cost(float* energies, int width, int row, float* min_energie
 * @param image the image
 * @return a 2d float matrix (height, width) representing the min energy of each pixel
 */
-__host__ Matrix compute_min_cost_mat_direct_cuda(Image& image) {
+__host__ void compute_min_cost_mat_direct_cuda(Image& image, int new_width) {
   find_cuda_device();
 
-  float height = image.height;
-  float width = image.width;
-  
-  float** image_data = image.image;
+  int height = image.height;
+  int width = image.width;
 
   //Send image to GPU
   const int num_points = height * width;
 
   //Allocate image
   float* image_flat;
-  cudaMalloc(&image_flat, num_points * NUM_COLORS * sizeof(float));
-  const int pixel_row_size = width * NUM_COLORS * sizeof(float);
-  for(int y = 0; y < height; ++y) {
-    int img_idx = y * width * NUM_COLORS;
-    cudaMemcpy(image_flat + img_idx, image_data[y], pixel_row_size, cudaMemcpyHostToDevice);
-  }
+  gpuErrchk(cudaMalloc(&image_flat, num_points * NUM_COLORS * sizeof(float)));
 
   //Allocate energies
   float* energies;
-  cudaMalloc(&energies, num_points * sizeof(float));
-  cudaMemset(energies, 0, num_points * sizeof(float));
+  gpuErrchk(cudaMalloc(&energies, num_points * sizeof(float)));
 
-  //Block decomposition
-  dim3 threads_per_block(BLOCK_DIM, BLOCK_DIM);
-  dim3 num_blocks((width + threads_per_block.x - 1) / threads_per_block.x,
-                  (height + threads_per_block.y - 1) / threads_per_block.y);
-
-  //launch kernel
-  dual_gradient<<<num_blocks, threads_per_block>>>(image_flat, height, width, energies);
-
-  //Allocate energies
   float* min_energies;
-  cudaMalloc(&min_energies, num_points * sizeof(float));
+  gpuErrchk(cudaMalloc(&min_energies, num_points * sizeof(float)));
 
-  //TODO new decomposition
-  int energy_blocks = width / THREADS_PER_BLOCK;
-  for(int y = 0; y < height; ++y) {  
-    min_cost<<<energy_blocks, THREADS_PER_BLOCK>>>(energies, width, y, min_energies);
+  int temp_width = width;
+  int pixel_row_size = temp_width * NUM_COLORS * sizeof(float);
+  while(temp_width > new_width) {
+    float** image_data = image.image;
+
+    for(int r = 0; r < height; ++r) {
+      int img_idx = r * temp_width * NUM_COLORS;
+      cudaMemcpy(image_flat + img_idx, image_data[r], pixel_row_size, cudaMemcpyHostToDevice);
+    }
+    cudaMemset(energies, 0, num_points * sizeof(float));
+
+    //Block decomposition
+    dim3 threads_per_block(BLOCK_DIM, BLOCK_DIM);
+    dim3 num_blocks((temp_width + threads_per_block.x - 1) / threads_per_block.x,
+                    (temp_width + threads_per_block.y - 1) / threads_per_block.y);
+
+    //launch kernel
+    dual_gradient<<<num_blocks, threads_per_block>>>(image_flat, height, temp_width, energies);
+
+    //TODO new decomposition  
+    int energy_blocks = (temp_width + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+    for(int y = 0; y < height; ++y) {  
+      min_cost<<<energy_blocks, THREADS_PER_BLOCK>>>(energies, temp_width, y, min_energies);
+    }
+
+    float** out_energies = (float**) malloc(height * sizeof(float*));
+    int row_size = temp_width * sizeof(float);
+    for(int y = 0; y < height; y++) {
+      out_energies[y] = (float*) malloc(row_size);
+      int offset = y * temp_width;
+      gpuErrchk(cudaMemcpy(out_energies[y], min_energies + offset, row_size, cudaMemcpyDeviceToHost));
+    }
+
+    Matrix min_cost_mat = Matrix(out_energies, height, temp_width);
+    int *seam = find_seam(min_cost_mat);
+    image.remove_seam(seam);
+
+    delete seam;
+    // delete &min_cost_mat;
+
+    temp_width--;
   }
 
-  float** out_energies = (float**)malloc(height * sizeof(float*));
-  const int row_size = width * sizeof(float);
-  for(int y = 0; y < height; y++) {
-    out_energies[y] = (float*)malloc(width * sizeof(float));
-    int offset = y * width;
-    cudaMemcpy(out_energies[y], min_energies + offset, row_size, cudaMemcpyDeviceToHost);
-  }
-
-  cudaFree(energies);
-  cudaFree(min_energies);
-
-  //Should be no need for barriers thanks to final cuda memcpy
-  return Matrix(out_energies, height, width);
+  gpuErrchk(cudaFree(image_flat));
+  gpuErrchk(cudaFree(energies));
+  gpuErrchk(cudaFree(min_energies));
 }
+
+//
+// non-direct stuff
+//
 
 __host__ Matrix compute_energy_mat_cuda(Image& image) {
   find_cuda_device();
 
-  float height = image.height;
-  float width = image.width;
+  int height = image.height;
+  int width = image.width;
   float** image_data = image.image;
 
   //Send image to GPU
@@ -194,11 +216,12 @@ __host__ Matrix compute_energy_mat_cuda(Image& image) {
   cudaMemset(energies, 0, num_points * sizeof(float));
 
   //Block decomposition
-  dim3 block_dims(ceil((double)height / BLOCK_DIM), ceil((double)width / BLOCK_DIM), 1);
-  dim3 thread_dims(BLOCK_DIM, BLOCK_DIM, 1);
+  dim3 threads_per_block(BLOCK_DIM, BLOCK_DIM);
+  dim3 num_blocks((width + threads_per_block.x - 1) / threads_per_block.x,
+                  (height + threads_per_block.y - 1) / threads_per_block.y);
 
   //launch kernel
-  dual_gradient<<<block_dims, thread_dims>>>(image_flat, height, width, energies);
+  dual_gradient<<<num_blocks, threads_per_block>>>(image_flat, height, width, energies);
 
   float** out_energies = (float**) malloc(height * sizeof(float*));
   const int row_size = width * sizeof(float);
@@ -209,6 +232,7 @@ __host__ Matrix compute_energy_mat_cuda(Image& image) {
   }
 
   cudaFree(energies);
+  gpuErrchk(cudaFree(image_flat));
 
   //Should be no need for barriers thanks to final cuda memcpy
   return Matrix(out_energies, height, width);
@@ -217,8 +241,8 @@ __host__ Matrix compute_energy_mat_cuda(Image& image) {
 __host__ Matrix compute_min_cost_mat_cuda(Matrix& energies_mat) {
   find_cuda_device();
 
-  float height = energies_mat.height;
-  float width = energies_mat.width;
+  int height = energies_mat.height;
+  int width = energies_mat.width;
   float** matrix = energies_mat.matrix;
 
   const int num_points = height * width;
@@ -239,13 +263,13 @@ __host__ Matrix compute_min_cost_mat_cuda(Matrix& energies_mat) {
   cudaMalloc(&min_energies, num_points * sizeof(float));
 
   //TODO new decomposition  
-  int energy_blocks = width / THREADS_PER_BLOCK;
+  int energy_blocks = (width + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
   for(int y = 0; y < height; ++y) {  
     min_cost<<<energy_blocks, THREADS_PER_BLOCK>>>(energies, width, y, min_energies);
   }
 
   float** out_energies = (float**) malloc(height * sizeof(float*));
-  for(int y = 0; y < height; ++y) {
+  for(int y = 0; y < height; y++) {
     out_energies[y] = (float*) malloc(width * sizeof(float));
     int offset = y * width;
     cudaMemcpy(out_energies[y], min_energies + offset, row_size, cudaMemcpyDeviceToHost);
